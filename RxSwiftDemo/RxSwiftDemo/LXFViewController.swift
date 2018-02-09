c7ad7dce40a7c8b48251da423bc5928cd88dd0d5
anchoriteFili <feiliyaailiya1@ilcoud.com>
2018-02-09 14:15:19 +0800

    提交一下

--- RxSwiftDemo/RxSwiftDemo/LXFViewController.swift
+++ RxSwiftDemo/RxSwiftDemo/LXFViewController.swift
@@ -32,6 +32,15 @@ class LXFViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+        
+        bindView()
+        
+        // 加载数据
+        
+        tableView.mj_header.beginRefreshing()
+        
 
         // Do any additional setup after loading the view.
     }
@@ -57,14 +66,76 @@ class LXFViewController: UIViewController {
 extension LXFViewController {
     
     fileprivate func setupUI() {
-        
-        
-        
-        
-        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            
+            make.left.right.bottom.equalTo(view)
+            make.top.equalTo(view.snp.top).offset(20)
+        }
     }
     
     
+    fileprivate func bindView() {
+        
+        // 绑定cell
+        dataSource.configureCell = { ds, tv, ip, item in
+            
+            let cell = tv.dequeueReusableCell(for: ip) as LXFViewCell
+            
+            cell.picView.kf.setImage(with: URL(string: item.url))
+            
+            cell.descLabel.text = "描述：\(item.desc)"
+            cell.sourceLabel.text = "来源： \(item.source)"
+            
+            return cell
+        }
+        
+        // 设置代理
+        tableView.rx.setDelegate(self).addDisposableTo(rx_disposeBag)
+        
+        
+        let vmInput = LXFViewModel.LXFInput(category: .welfare)
+        let vmOutput = viewModel.transform(input: vmInput)
+        
+        
+        vmOutput.sections.asDriver().drive(tableView.rx.items(dataSource: dataSource)).addDisposableTo(rx_disposeBag)
+        
+        
+        vmOutput.refreshStates.asObservable().subscribe(onNext: { [weak self] status in
+            
+            switch status {
+            case .beingHeaderRefresh:
+                self?.tableView.mj_header.beginRefreshing()
+            case .endHeaderRefresh:
+                self?.tableView.mj_header.endRefreshing()
+            case .beingFooterRefresh:
+                self?.tableView.mj_footer.beginRefreshing()
+            case .endFooterRefresh:
+                self?.tableView.mj_footer.endRefreshing()
+            case .noMoreData:
+                self?.tableView.mj_footer.endRefreshingWithNoMoreData()
+            default:
+                break
+            }
+        }).addDisposableTo(rx_disposeBag)
+        
+        
+        tableView.mj_header = MJRefreshNormalHeader(refreshingBlock: {
+            vmOutput.requestCommond.onNext(true)
+        })
+        
+        tableView.mj_footer = MJRefreshAutoNormalFooter(refreshingBlock: {
+            vmOutput.requestCommond.onNext(false)
+        })
+    }
+    
+}
+
+extension LXFViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 }
 
